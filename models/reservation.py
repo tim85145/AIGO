aa7d93842cfa45e1b1f82f2ff02d1c3634b3d504
tim85145 <tim85145@gmail.com>
2023-08-19 11:12:49 +0800

    Add files via upload

--- models/reservation.py
+++ models/reservation.py
@@ -1,20 +1,19 @@
 from extensions import db
 import datetime
 
+
 class Reservation(db.Model):
     __tablename__ = 'reservation'
 
-    id = db.Column(db.Integer(), primary_key=True)
-    user_id = db.Column(db.Integer(), db.Foreign("user.id"))
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     booking_service_category = db.Column(db.String(50), nullable=False)
     booking_service = db.Column(db.String(150), nullable=False)
-    booking_datetime = db.Column(db.Datetime, nullable=False)
+    booking_datetime = db.Column(db.DateTime, nullable=False)
 
     is_canceled = db.Column(db.Boolean(), server_default='0')
-    created_on = db.Column(db.Datetime, default=datetime.datetime.now())
-    update_on = db.Column(db.Datetime, default=datetime.datetime.now(), onupdate=datetime.datetime.now())
-    
-    created_on = db.Column(db.Datetime, default=datetime.datetime.now())
+    created_on = db.Column(db.DateTime, default=datetime.datetime.now())
+    updated_on = db.Column(db.DateTime, default=datetime.datetime.now(), onupdate=datetime.datetime.now())
 
     def __init__(self, user_id, booking_service_category, booking_service, booking_datetime):
         self.user_id = user_id
--- models/user.py
+++ models/user.py
@@ -1,19 +1,19 @@
 from extensions import db
 import datetime
 
+
 class User(db.Model):
     __tablename__ = 'user'
 
-    id = db.Column(db.Integer(), primary_key=True)
-    
-    line_id = db.Column(db.String(50), unique=True)
-    display_name = db.Column(db.String(255))
-    picture_url = db.Column(db.String(255))
-    
-    created_on = db.Column(db.Datetime, default=datetime.datetime.now())
+    id = db.Column(db.Integer, primary_key=True)#設定它為主鍵
+
+    line_id = db.Column(db.String(50), unique=True)#unique=True代表是不可以重複的
+    display_name = db.Column(db.String(255))#用戶的line名稱
+    picture_url = db.Column(db.String(255))#大頭貼的url
+
+    created_on = db.Column(db.DateTime, default=datetime.datetime.now())
 
     def __init__(self, line_id, display_name, picture_url):
         self.line_id = line_id
         self.display_name = display_name
         self.picture_url = picture_url
-    
