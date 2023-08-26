d06a9b11db48fd7a67d9aa583dc147f4531b1165
tim85145 <tim85145@gmail.com>
2023-08-26 10:16:56 +0800

    Add files via upload

--- events/service.py
+++ events/service.py
@@ -247,7 +247,8 @@ def service_confirm_event(event):
                 ),
                 MessageAction(
                     label='重新預約',
-                    text='我想重新預約'
+                    text='我想重新預約',
+                    data='action=select_date'
                 )
             ]
         )
